74b76f1f2261f389bd2c3e71bef64e90c75b0281
Harrison Corupe <harrisoncorupe@Harrisons-MacBook-Pro-2.local>
2021-08-20 15:14:03 -0700

    First commit

---
+++ android/app/src/main/kotlin/com/example/value_notifier_dark_mode/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.value_notifier_dark_mode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+final _darkNotifier = ValueNotifier<bool>(false);
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+        valueListenable: _darkNotifier,
+        builder: (BuildContext context, bool isDark, Widget? child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
+            theme: ThemeData(primaryColor: Colors.blue),
+            darkTheme: ThemeData.dark(),
+            home: const MyHomePage(
+              title: 'App Title',
+            ),
+          );
+        });
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _darkNotifier.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isDark = _darkNotifier.value;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Press Button to Toggle Light/Dark mode',
+            ),
+            Text(
+              _darkNotifier.value ? 'DarkMode' : 'LightMode',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print(_darkNotifier.value.toString());
+          isDark = !isDark;
+          _darkNotifier.value = isDark;
+        },
+        tooltip: 'Press',
+        child: Icon(isDark ? Icons.wb_sunny_outlined : Icons.bubble_chart),
+      ),
+    );
+  }
+}
